612a63520f98db38a810fcc47166f6cd5b6fa3a8
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-15 06:16:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ mv cloudsinstaller/settings3storage ~/
 mv cloudsinstaller/removeowncloud ~/
 mv cloudsinstaller/uploadsetting ~/
 mv cloudsinstaller/fixsindex ~/
+mv cloudsinstaller/Restorenexcloud ~/
+mv cloudsinstaller/BackupNexcloud ~/
 
 # Remove the cloned directory
 rm -rf cloudsinstaller
@@ -21,6 +23,8 @@ chmod +x ~/settings3storage
 chmod +x ~/removeowncloud
 chmod +x ~/uploadsetting
 chmod +x ~/fixsindex
+chmod +x ~/BackupNexcloud
+chmod +x ~/Restorenexcloud
 
 # Move scripts to /usr/local/bin for easy access
 mv ~/menuclouds /usr/local/bin
@@ -29,6 +33,8 @@ mv ~/settings3storage /usr/local/bin
 mv ~/removeowncloud /usr/local/bin
 mv ~/uploadsetting /usr/local/bin
 mv ~/fixsindex /usr/local/bin
+mv ~/BackupNexcloud /usr/local/bin
+mv ~/Restorenexcloud /usr/local/bin
 
 # Add 'menuclouds' command to bashrc for auto-launch
 echo "menuclouds" >> ~/.bashrc
