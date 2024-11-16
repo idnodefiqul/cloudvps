0513b5e84ae550e9ad11df206bac91dae13a1fef
KangQull <110822952+KangQull@users.noreply.github.com>
2024-11-16 07:11:45 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -9,6 +9,7 @@ mv cloudsv2/hapus_nextcloud ~/
 mv cloudsv2/menucloudv2 ~/
 mv cloudsv2/ncloudv2 ~/
 mv cloudsv2/restorev2 ~/
+mv cloudsv2/settings3storage ~/
 
 # Remove the cloned directory
 rm -rf cloudsv2
@@ -19,6 +20,7 @@ chmod +x ~/ncloudv2
 chmod +x ~/menucloudv2
 chmod +x ~/hapus_nextcloud
 chmod +x ~/backupv2
+chmod +x ~/settings3storage
 
 # Move scripts to /usr/local/bin for easy access
 mv ~/menucloudv2 /usr/local/bin
@@ -26,6 +28,7 @@ mv ~/hapus_nextcloud /usr/local/bin
 mv ~/backupv2 /usr/local/bin
 mv ~/ncloudv2 /usr/local/bin
 mv ~/restorev2 /usr/local/bin
+mv ~/settings3storage /usr/local/bin
 
 # Add 'menuclouds' command to bashrc for auto-launch
 echo "menucloudv2" >> ~/.bashrc
